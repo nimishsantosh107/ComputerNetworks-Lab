42e93eb36127d55cf62f5ccfae18d18539e1756b
Nimish Santosh <nimishsantosh107@icloud.com>
2019-06-25 21:46:40 +0530

    changes

--- network.c
+++ network.c
@@ -1,8 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+
+int error(char *msg){
+	perror(msg);
+	exit(1);
+}
 
 int main(int argc, char const *argv[]){
 	int server_fd;
@@ -10,10 +16,9 @@ int main(int argc, char const *argv[]){
 	char buffer[1024] = {0}; 
 
 	//CREATE SOCKET
-	if((server_fd = socket(PF_INET,SOCK_STREAM,0)) == 0){
-		perror("SOCKET FAILED");
-		exit(1);
-	}
+	if((server_fd = socket(PF_INET,SOCK_STREAM,0)) == 0)
+		error("SOCKET FAILED");
+	
 	printf("%d\n",server_fd);
 
 	//CLOSE SOCKET
