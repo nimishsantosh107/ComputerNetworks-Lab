e7c31abaf6c21de1b4aaf56dbcff5e0526d44558
Nimish Santosh <nimishsantosh107@icloud.com>
2019-06-26 22:22:18 +0530

    colorized TCP-IP

--- TCP-IP/client.c
+++ TCP-IP/client.c
@@ -21,14 +21,14 @@ void chat(int sock_fd){
 	
 	while(1){
 		//MESSAGE
-		printf("YOU: ");
+		printf("\033[1;34mYOU:\033[0m ");
 		n=0;
 		while ((buf[n++] = getchar()) != '\n')
 			;
 		write(sock_fd, buf, sizeof(buf));
 		if ((strncmp(buf, "exit", 4)) == 0) { 
 			bzero(buf, MAX);
-			printf("CLIENT EXIT\n"); 
+			printf("\033[1;31mCLIENT EXIT\033[0m\n"); 
 			break; 
 		} 
 		bzero(buf, MAX);
@@ -36,10 +36,10 @@ void chat(int sock_fd){
 		read(sock_fd ,&buf, sizeof(buf));
 		if ((strncmp(buf, "exit", 4)) == 0) { 
 			bzero(buf, MAX);
-			printf("SERVER EXIT\n"); 
+			printf("\033[1;31mSERVER EXIT\033[0m\n"); 
 			break; 
 		} 
-		printf("SERVER: %s\n", buf);
+		printf("\033[1;34mSERVER:\033[0m %s\n", buf);
 		bzero(buf, MAX);
 	}
 }
@@ -51,16 +51,16 @@ int main(int argc, char const *argv[])
 	struct sockaddr_in servaddr; 
 
 	//CREATE SOCKET
-	if ((sock_fd = socket(PF_INET,SOCK_STREAM,0)) <= 0) error("SOCKET FAILED");
+	if ((sock_fd = socket(PF_INET,SOCK_STREAM,0)) <= 0) error("\033[0;31mSOCKET FAILED\033[0m");
 
 	//RESET servaddr / ASSIGN IP, PORT, FAMILY  OF **SERVER**
 	bzero(&servaddr, sizeof(servaddr)); 
-	if ((inet_aton("127.0.0.1", &servaddr.sin_addr)) == 0) error("IP ERROR");
+	if ((inet_aton("127.0.0.1", &servaddr.sin_addr)) == 0) error("\033[0;31mIP ERROR\033[0m");
 	servaddr.sin_port = htons(SERVER_PORT); 
 	servaddr.sin_family = PF_INET; 
 
     //CONNECT
-	if ((connect(sock_fd, (SA*)&servaddr, sizeof(servaddr))) != 0) error("CONNECT ERROR");
+	if ((connect(sock_fd, (SA*)&servaddr, sizeof(servaddr))) != 0) error("\033[0;31mCONNECT ERROR\033[0m");
 	
     //CHAT
 	chat(sock_fd);
--- TCP-IP/server.c
+++ TCP-IP/server.c
@@ -24,20 +24,21 @@ void chat(int client_fd){
 		read(client_fd, buf, sizeof(buf));
 		if ((strncmp(buf, "exit", 4)) == 0) { 
 			bzero(buf, MAX); 
-			printf("CLIENT EXIT\n"); 
+			printf("\033[1;31mCLIENT EXIT\033[0m\n"); 
 			break; 
 		} 
-		printf("CLIENT: %s", buf);
+		printf("\033[1;34mCLIENT:\033[0m %s", buf);
 		bzero(buf, MAX); 
 		//MESSAGE
-		printf("YOU: ");
+		printf("\033[1;34mYOU:\033[0m ");
 		n=0;
 		while ((buf[n++] = getchar()) != '\n') 
 			;
 		write(client_fd, buf, sizeof(buf));
 		if ((strncmp(buf, "exit", 4)) == 0) { 
 			bzero(buf, MAX);
-			printf("SERVER EXIT\n"); 
+			printf("");
+			printf("\033[1;31mSERVER EXIT\033[0m\n"); 
 			break; 
 		} 
 		printf("\n");
@@ -51,24 +52,24 @@ int main(int argc, char const *argv[]){
 	struct sockaddr_in servaddr, clientaddr; 	
 
 	//CREATE SOCKET
-	if ((server_fd = socket(PF_INET,SOCK_STREAM,0)) <= 0) error("SOCKET FAILED");
+	if ((server_fd = socket(PF_INET,SOCK_STREAM,0)) <= 0) error("\033[0;31mSOCKET FAILED\033[0m");
 
 	//RESET servaddr / ASSIGN IP, PORT, FAMILY 
 	bzero(&servaddr, sizeof(servaddr)); 
-	if ((inet_aton("127.0.0.1", &servaddr.sin_addr)) == 0) error("IP ERROR");
+	if ((inet_aton("127.0.0.1", &servaddr.sin_addr)) == 0) error("\033[0;31mIP ERROR\033[0m");
 	servaddr.sin_port = htons(SERVER_PORT); 
 	servaddr.sin_family = PF_INET; 
 
     //BIND TO PORT
-	if ((bind(server_fd, (SA*)&servaddr, sizeof(servaddr))) != 0) error("BIND ERROR");
+	if ((bind(server_fd, (SA*)&servaddr, sizeof(servaddr))) != 0) error("\033[0;31mBIND ERROR\033[0m");
 
     //LISTEN ON PORT
-	if ((listen(server_fd, 5)) != 0) error("LISTEN ERROR");
-	printf("SERVER LISTENING ON PORT 4000\n\n");
+	if ((listen(server_fd, 5)) != 0) error("\033[0;31mLISTEN ERROR\033[0m");
+	printf("\033[1;36mSERVER LISTENING ON PORT 4000\033[0m\n\n");
 
     //ACCEPT CONNECTION
 	unsigned int len = sizeof(clientaddr);
-	if ((client_fd = accept(server_fd, (SA*)&clientaddr, &len)) <= 0) error("ACCEPT ERROR"); 
+	if ((client_fd = accept(server_fd, (SA*)&clientaddr, &len)) <= 0) error("\033[0;31mACCEPT ERROR\033[0m"); 
 
     //CHAT
 	chat(client_fd);
